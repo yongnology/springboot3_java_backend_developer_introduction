3daab466de698c92147b0aebf01b1f34886621e1
yongnology <cassius20@naver.com>
2024-06-28 00:36:52 +0900

    3.2.3 폼 데이터 전송하기_ 3.2.4 폼 데이터 받기

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -9,5 +10,10 @@ public class ArticleController {
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle() {
+        return "";
     }
 }
