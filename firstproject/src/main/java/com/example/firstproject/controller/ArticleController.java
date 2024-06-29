562bf3c75af51101a820a5aef6f852a77e4e71aa
yongnology <cassius20@naver.com>
2024-06-29 22:49:25 +0900

    3.3.3 리파지터리로 엔티티를 DB에 저장하기

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,12 +2,17 @@ package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -19,7 +24,11 @@ public class ArticleController {
         System.out.println(form.toString() );   // DTO에 폼 데이터가 잘 담겼는지 확인
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
+        System.out.println(article.toString() );    // DTO가 엔티티로 잘 변환되는지 확인 출력
+
         // 2. 리파지터리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);    // article 엔티티를 저장해 saved 객체에 반환
+        System.out.println(saved.toString() );      // article이 DB에 잘 저장되는지 확인 출력
         return "";
     }
 }
---
+++ firstproject/src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.Article;
+// CrudRepository 패키지 자동 임포트
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+
+}
