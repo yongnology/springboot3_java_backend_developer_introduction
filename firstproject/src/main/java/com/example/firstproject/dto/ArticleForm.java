a4e7cb858ba4ff740d89bf911a963b4155ccdb66
yongnology <cassius20@naver.com>
2024-06-29 23:22:05 +0900

    4.2.2 DTO 리팩터링하기

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,25 +1,31 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     
     private String title;   // 제목을 받을 필드
     private String content; // 내용을 받을 필드
 
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
+// @AllArgsConstructor 추가하면서 주석
+//    public ArticleForm(String title, String content) {
+//        this.title = title;
+//        this.content = content;
+//    }
 
-    // 데이터를 잘 받았는지 확인한 toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+// @ToString 추가하면서 주석
+//// 데이터를 잘 받았는지 확인한 toString() 메서드 추가
+//@Override
+//public String toString() {
+//    return "ArticleForm{" +
+//            "title='" + title + '\'' +
+//            ", content='" + content + '\'' +
+//            '}';
+//}
 
     public Article toEntity() {
         return new Article(null, title, content);
