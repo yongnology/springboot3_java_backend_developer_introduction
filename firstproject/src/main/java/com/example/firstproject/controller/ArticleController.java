324215ce4c7945b12f3e7c8c040579297efc4d90
yongnology <cassius20@naver.com>
2024-07-01 23:03:39 +0900

    4.2.3 엔티티 리팩터링하기, 4.2.4 컨트롤러에 로그 남기기

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j  // 로깅 기능을 위한 어노테이션 추가
 @Controller
 public class ArticleController {
 
@@ -21,14 +23,17 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString() );   // DTO에 폼 데이터가 잘 담겼는지 확인
+//        System.out.println(form.toString() );   // DTO에 폼 데이터가 잘 담겼는지 확인
+        log.info(form.toString() );
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString() );    // DTO가 엔티티로 잘 변환되는지 확인 출력
+//        System.out.println(article.toString() );    // DTO가 엔티티로 잘 변환되는지 확인 출력
+        log.info(article.toString());
 
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);    // article 엔티티를 저장해 saved 객체에 반환
-        System.out.println(saved.toString() );      // article이 DB에 잘 저장되는지 확인 출력
+//        System.out.println(saved.toString() );      // article이 DB에 잘 저장되는지 확인 출력
+        log.info(saved.toString());
         return "";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -4,9 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 public class Article {
+
     @Id
     @GeneratedValue
     private Long id;
@@ -17,20 +24,20 @@ public class Article {
     @Column
     private String content;
 
-    // Article 생성자 추가
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    // toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    // Article 생성자 추가
+//    public Article(Long id, String title, String content) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//    }
+//
+//    // toString() 메서드 추가
+//    @Override
+//    public String toString() {
+//        return "Article{" +
+//                "id=" + id +
+//                ", title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 }
