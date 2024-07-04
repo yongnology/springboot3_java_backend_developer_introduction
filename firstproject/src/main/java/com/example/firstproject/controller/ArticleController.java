9004e6ae442d83312741344da59de7054ccfbe69
yongnology <cassius20@naver.com>
2024-07-05 00:38:11 +0900

    7.2.2 Edit 요청을 받아 데이터 가져오기 , 7.2.3 수정 폼 만들기

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -66,4 +66,16 @@ public class ArticleController {
         // 3. 사용자에게 보여 줄 뷰 페이지 설정하기
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // 수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        // 뷰 페이지 설정하기
+        return "articles/edit";
+    }
 }
