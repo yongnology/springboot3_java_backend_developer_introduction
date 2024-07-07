5cbbc26cf2b9511f7f5e06bc65edacff9210d19f
yongnology <cassius20@naver.com>
2024-07-07 18:34:30 +0900

    7.3.5 DB에 저장하고 결과 페이지로 리다이렉트하기

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -82,6 +82,19 @@ public class ArticleController {
     @PostMapping("/articles/update")
     public String update(ArticleForm form) {
         log.info(form.toString() );
-        return "";
+        // 1. DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2. 엔티티를 DB에 저장하기
+        // 2-1 DB에서 기존 데이터 가져오기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        // 2-2 기존 데이터 값을 갱신하기
+        if(target != null) {
+            articleRepository.save(articleEntity);  // 엔티티를 DB에 저장(갱신)
+        }
+
+        // 3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/articles/" + articleEntity.getId();
     }
 }
